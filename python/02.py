220cc2617b3d847badb555b8b910ff7302d81431
Ian Sweet <Iannsweet@gmail.com>
2014-08-31 13:27:21 -0700

    problem 2, python

---
+++ python/02.py
@@ -0,0 +1,9 @@
+def fib_gen(lim):
+    first = 1
+    second = 2
+
+    while (first <= lim):
+        yield first
+	first, second = second, first + second
+
+print sum(filter(lambda x: x % 2 == 0, fib_gen(4000000)))
